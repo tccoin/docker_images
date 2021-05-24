1a5901c4d915e58141adbc2bf46c26b3fc7fe76c
zeroAska <trotwoodray@gmail.com>
2021-05-24 18:03:02 -0400

    nvidia visualization

---
+++ visualization/run_cuda_docker.bash
@@ -0,0 +1,19 @@
+container_name=$1
+
+xhost +local:
+docker run -it --net=host --gpus all \
+  --user=$(id -u) \
+  --workdir=/home/$USER/ \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v "/etc/passwd:/etc/passwd:rw" \
+  -e "TERM=xterm-256color" \
+  --env="QT_X11_NO_MITSHM=1" \
+  -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY=$DISPLAY -e USER=$USER \
+  -v "/home/$USER/code/docker_visualization_home:/home/$USER/" \
+  -v "/run/media/$USER/Samsung_T5/:/home/$USER/code/docker_home/media/Samsung_T5" \
+  --device=/dev/dri:/dev/dri \
+  --name=${container_name} \
+  umrobotics/visualization:latest
+
+
+  #--env="QT_X11_NO_MITSHM=1"  -e QT_GRAPHICSSYSTEM=native #-e XAUTHORITY \
