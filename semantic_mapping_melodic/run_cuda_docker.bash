ec011098a452db4834e17813a3445be0adf164c1
zeroAska <trotwoodray@gmail.com>
2022-01-25 00:06:01 -0500

    update old semanticmapping dockerfile

--- semantic_mapping_melodic/run_cuda_docker.bash
+++ semantic_mapping_melodic/run_cuda_docker.bash
@@ -1,19 +1,19 @@
 container_name=$1
 
 xhost +local:
-docker run -it --net=host --runtime=nvidia \
+docker run -it --net=host --gpus all \
   --user=$(id -u) \
   -e DISPLAY=$DISPLAY \
   -e QT_GRAPHICSSYSTEM=native \
   -e CONTAINER_NAME=cuda \
+  -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -e XAUTHORITY \
   -e USER=$USER \
   --workdir=/home/$USER \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  -v "/etc/group:/etc/group:ro" \
-  -v "/etc/passwd:/etc/passwd:ro" \
-  -v "/etc/shadow:/etc/shadow:ro" \
-  -v "/etc/sudoers.d:/etc/sudoers.d:ro" \
-  -v "/home/$USER/:/home/$USER/" \
-  --device=/dev/dri:/dev/dri \
+   -v "/etc/passwd:/etc/passwd:ro" \
+   -v "/home/$USER/code/docker_home/visualization/:/home/$USER/" \
+   -v "/media/sda1/:/home/$USER/sda1" \
+   --device=/dev/dri:/dev/dri \
   --name=${container_name} \
   umrobotics/semantic_mapping_melodic:latest
