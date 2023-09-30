eae52509268492a0aee5202fca37df40ec2a72d7
tccoin <junzhewu@umich.edu>
2023-09-30 01:55:09 -0400

    update slam container

--- cvo_gpu/run_cuda_docker.bash
+++ cvo_gpu/run_cuda_docker.bash
@@ -12,7 +12,18 @@ docker run -it --net=host --gpus all  -e DISPLAY=${DISPLAY} \
   -u $(id -u):$(id -g) --workdir="/home/$USER/" \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"  -v "/etc/passwd:/etc/passwd:rw" -v  "/etc/group:/etc/group:ro"  -v "/etc/shadow:/etc/shadow:ro" -e "TERM=xterm-256color" \
   --device /dev/nvidia0 --device /dev/nvidia-modeset --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia-uvm-tools  \
-  -v "/home/$USER/code/docker_home/cvo/:/home/$USER/" \
-  -v "/run/media/$USER/Samsung_T5/:/home/$USER/media/Samsung_T5" \
-  -v "/run/media/$USER/Samsung_T5_2/:/home/$USER/media/Samsung_T5_2" --name=${container_name}  umrobotics/cvo:latest
+  -v "/home/link/Projects/:/home/$USER/" \
+  -v /run/media/link/Samsung_T5/:/dataset \
+  --name=cvo_stack  umcurly/cvo:latest
 
+# docker run -it --net=host --gpus all  -e DISPLAY=${DISPLAY} \
+#   -e QT_GRAPHICSSYSTEM=native \
+#   -e NVIDIA_DRIVER_CAPABILITIES=all \
+#   -e XAUTHORITY \
+#   -v /etc/group:/etc/group:ro \
+#   -u $(id -u):$(id -g) --workdir="/home/$USER/" \
+#   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"  -v "/etc/passwd:/etc/passwd:rw" -v  "/etc/group:/etc/group:ro"  -v "/etc/shadow:/etc/shadow:ro" -e "TERM=xterm-256color" \
+#   --device /dev/nvidia0 --device /dev/nvidia-modeset --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia-uvm-tools  \
+#   -v /home/$USER/unified_cvo/:/home/$USER/ \
+#   -v /media/sdg1/rzh/:/dataset \
+#   --name=cvo_stack umrobotics/cvo:latest
