b3daf796a184845337a0e5e0a28c48c56c0df59c
Junzhe Wu <junzhewu@umich.edu>
2024-03-01 01:50:20 -0600

    use torch2.2 for nerf slam (for gpu profiling)

--- nerf_slam/build.sh
+++ nerf_slam/build.sh
@@ -1,1 +1,2 @@
-docker build --build-arg USER=$USER --tag tccoin/nerf_slam .
+# docker build --build-arg USER=$USER --tag tccoin/nerf_slam .
+docker build --build-arg USER=$USER --tag tccoin/nerf_slam:torch_2_2 -f ./Dockerfile_torch_2_2 .
