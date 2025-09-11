5146a9680904d65c2b4aea32e7286f0ea259bf11
tccoin <junzhewu@umich.edu>
2025-09-11 18:18:43 -0400

    update isaac version

--- sg_vln/build.sh
+++ sg_vln/build.sh
@@ -5,4 +5,4 @@ docker pull nvcr.io/nvidia/isaac-sim:4.5.0
 # https://pyojuncode-github-io.translate.goog/Docker-using-ssh-in-builds/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp
 # DOCKER_BUILDKIT=1 docker build --tag curly/vln_game:latest --ssh default .
 # DOCKER_BUILDKIT=1 docker build --progress=plain --tag curly/sg_vln:habitat3.3 .
-docker build --tag curly/sg_vln:latest .
+docker build --progress=plain --tag curly/sg_vln:latest .
